9420c160bace12fa14543ae1eb00e6bd61afccb3
Dzana Rahic <rahic.dzana@ljuca.de>
2022-04-28 15:10:38 +0200

    ....

---
+++ src/main/java/de/hfu/Stringaufrufen.java
@@ -0,0 +1,24 @@
+package de.hfu;
+
+import java.util.*;
+
+public class Stringaufrufen {
+
+public static void main(String[] args) {
+
+Scanner scan = new Scanner(System.in);
+System.out.print("Eingabe: ");
+String Eingabe = scan.next();
+for(char c : Eingabe.toCharArray()){
+if(Character.isLowerCase(c)){
+System.out.print( Character.toUpperCase(c));
+} 
+else {
+System.out.println("Deine Eingabe ist: " + Eingabe + c);
+
+			}
+		}
+
+	}
+
+}
